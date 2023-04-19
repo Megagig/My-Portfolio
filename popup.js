d4800c84764abbd5143e994d34d41396b87a7759
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-04-19 01:09:10 +0100

    Refactor the card section

--- popup.js
+++ popup.js
@@ -1,167 +1,165 @@
-const projectsSide = document.querySelector('.popuptop');
-const modal = document.querySelector('#modal');
+// const projectsSide = document.querySelector('.projects');
+// const modal = document.querySelector('#modal');
 
-const projects = [
-  {
-    id: 1,
-    name: 'Tonic',
-    desc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featureImg: 'images/Snapshoot Portfolio.png',
-    technology: ['html', 'css', 'javascript'],
-    platform: 'CANOPY',
-    stack: 'Back End Dev',
-    year: 2015,
-    liveLink: 'https://megagig.github.io/My-Portfolio/',
-    linkSource: 'https://github.com/Megagig/My-Portfolio',
-  },
-  {
-    id: 2,
-    name: 'Multi-Post Stories',
-    desc: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    featureImg: 'assets/images/Snapshoot-portfolio-4.png',
-    technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
-    platform: 'FACEBOOK',
-    stack: 'Full Stack Dev',
-    year: 2015,
-    liveLink: '#',
-    linkSource: '#',
-  },
-  {
-    id: 3,
-    name: 'Facebook 360',
-    desc: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    featureImg: 'assets/images/Snapshoot-portfolio-1.png',
-    technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
-    platform: 'FACEBOOK',
-    stack: 'Full Stack Dev',
-    year: 2015,
-    liveLink: 'https://megagig.github.io/My-Portfolio/',
-    linkSource: 'https://github.com/Megagig/My-Portfolio',
-  },
-  {
-    id: 4,
-    name: 'Uber Navigation',
-    desc: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    featureImg: 'assets/images/Snapshoot-portfolio-2.png',
-    technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
-    platform: 'Uber',
-    stack: 'Lead Developer',
-    year: 2018,
-    liveLink: 'https://megagig.github.io/My-Portfolio/',
-    linkSource: 'https://github.com/Megagig/My-Portfolio',
-  },
+// const projects = [
+//   {
+//     id: 1,
+//     name: 'Tonic',
+//     desc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+//     featureImg: 'images/snapshoot1.png',
+//     technology: ['html', 'css', 'javascript'],
+//     platform: 'CANOPY',
+//     stack: 'Back End Dev',
+//     year: 2015,
+//     liveLink: 'https://megagig.github.io/My-Portfolio/',
+//     linkSource: 'https://github.com/Megagig/My-Portfolio',
+//   },
+//   {
+//     id: 2,
+//     name: 'Multi-Post Stories',
+//     desc: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+//     featureImg: 'images/snapshoot2.png',
+//     technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
+//     platform: 'FACEBOOK',
+//     stack: 'Full Stack Dev',
+//     year: 2015,
+//     liveLink: '#',
+//     linkSource: '#',
+//   },
+//   {
+//     id: 3,
+//     name: 'Facebook 360',
+//     desc: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+//     featureImg: 'images/card2.svg',
+//     technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
+//     platform: 'FACEBOOK',
+//     stack: 'Full Stack Dev',
+//     year: 2015,
+//     liveLink: 'https://megagig.github.io/My-Portfolio/',
+//     linkSource: 'https://github.com/Megagig/My-Portfolio',
+//   },
+//   {
+//     id: 4,
+//     name: 'Uber Navigation',
+//     desc: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+//     featureImg: 'images/Snapshoot-portfolio-2.png',
+//     technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
+//     platform: 'Uber',
+//     stack: 'Lead Developer',
+//     year: 2018,
+//     liveLink: 'https://megagig.github.io/My-Portfolio/',
+//     linkSource: 'https://github.com/Megagig/My-Portfolio',
+//   },
 
-  {
-    id: 5,
-    name: 'Portfolio Website',
-    desc: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    featureImg: 'assets/images/Snapshoot-portfolio-3.png',
-    technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
-    platform: 'Instagram',
-    stack: 'Full Stack Dev',
-    year: 2016,
-    liveLink: 'https://megagig.github.io/My-Portfolio/',
-    linkSource: 'https://github.com/Megagig/My-Portfolio',
-  },
-];
+//   {
+//     id: 5,
+//     name: 'Portfolio Website',
+//     desc: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+//     featureImg: 'images/card4.svg',
+//     technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
+//     platform: 'Instagram',
+//     stack: 'Full Stack Dev',
+//     year: 2016,
+//     liveLink: 'https://megagig.github.io/My-Portfolio/',
+//     linkSource: 'https://github.com/Megagig/My-Portfolio',
+//   },
+// ];
 
-function generateProduct() {
-  projectsSide.innerHTML = projects
-    .map(
-      (product) => `
-  <div class="popuptop">
-        <img src=${product.featureImg} alt="" />
+// function generateProduct() {
+//   projectsSide.innerHTML = projects
+//     .map(
+//       (product) => `
+//   <div class="projects">
+//         <img src=${product.featureImg} alt="" />
 
-        <div class="wrap">
-          <h2>${product.name}</h2>
+//         <div class="popuptop">
+//           <h2>${product.name}</h2>
 
-          <div class="front">
-            <p class="canopy">CANOPY</p>
-            div class="round"></div>
-            <p>${product.stack}</p>
-            div class="round"></div>
-            <p>${product.year}</p>
-          </div>
+//           <div class="front">
+//             <p class="canopy">CANOPY</p>
+//             <p>${product.stack}</p>
+//          <p>${product.year}</p>
+//           </div>
 
-          <p class="text">${product.desc}</p>
+//           <p class="text">${product.desc}</p>
 
-          <ul class="btns">
-            ${product.technology.map((x) => `<li>${x}</li>`).join('')}
-          </ul>
+//           <ul class="btns">
+//             ${product.technology.map((x) => `<li>${x}</li>`).join('')}
+//           </ul>
 
-          <a class="btn" id=${product.id} href="#">See projects</a>
-        </div>
-      </div>
-`
-    )
-    .join('');
-}
+//           <a class="btn" id=${product.id} href="#">See projects</a>
+//         </div>
+//       </div>
+// `
+//     )
+//     .join('');
+// }
 
-projectsSide.addEventListener('click', (e) => {
-  if (e.target.classList.contains('detail-btn')) {
-    modal.style.display = 'block';
-    const id = parseFloat(e.target.id);
-    const search = projects.find((x) => x.id === id);
-    if (search !== undefined) {
-      modal.innerHTML = `
-         <div class="popup"">
-          <div class="header-close modal-container">
-            <h3 class="popup-title">${search.name}</h3>
-            <i id="close-btn" class="uil uil-multiply"></i>
-          </div>
-          <div class="title-and-date modal-container">
-            <p class="title">${search.platform}</p>
-            <span></span>
-            <p class="muted">${search.stack}</p>
-            <span></span>
-            <p class="muted">${search.year}</p>
-          </div>
-  
-          <div class="card-info modal-container">
-            <img class="main-img" src=${search.featureImg} alt="" />
-  
-            <div class="switch">
-              <div class="description">
-                <p>
-                 ${search.desc}
-                </p>
-  
-                <p class="hide">
-                ${search.desc}
-                </p>
-              </div>
-  
-              <div class="stack-and-buttons">
-                <ul class="stack">
-  
-                    ${search.technology.map((x) => `<li>${x}</li>`).join('')}
-                </ul>
-  
-                <div class="buttons">
-                  <a href="#" class="see-live"
-                    ><img src="./images/Enabled1.png" alt="see live"
-                  /></a>
-  
-                  <a href="#" class="see-source"
-                    ><img src="./images/Enabled.png" alt="see source"
-                  /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      
-      `;
-    }
-  }
-  generateProduct();
-});
+// projectsSide.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('see-button')) {
+//     modal.style.display = 'block';
+//     const id = parseFloat(e.target.id);
+//     const search = projects.find((x) => x.id === id);
+//     if (search !== undefined) {
+//       modal.innerHTML = `
+//        <div class="popup"">
+//         <div class="header-close modal-container">
+//           <h3 class="popup-title">${search.name}</h3>
+//           <i id="close-btn" class="uil uil-multiply"></i>
+//         </div>
+//         <div class="title-and-date modal-container">
+//           <p class="title">${search.platform}</p>
+//           <span></span>
+//           <p class="muted">${search.stack}</p>
+//           <span></span>
+//           <p class="muted">${search.year}</p>
+//         </div>
 
-modal.addEventListener('click', (e) => {
-  if (e.target.id === 'close-btn') {
-    modal.style.display = 'none';
-  }
-  generateProduct();
-});
+//         <div class="card-info modal-container">
+//           <img class="main-img" src=${search.featureImg} alt="" />
 
-generateProduct();
+//           <div class="switch">
+//             <div class="description">
+//               <p>
+//                ${search.desc}
+//               </p>
+
+//               <p class="hide">
+//               ${search.desc}
+//               </p>
+//             </div>
+
+//             <div class="stack-and-buttons">
+//               <ul class="stack">
+
+//                   ${search.technology.map((x) => `<li>${x}</li>`).join('')}
+//               </ul>
+
+//               <div class="buttons">
+//                 <a href="#" class="see-live"
+//                   ><img src="./images/Enabled1.png" alt="see live"
+//                 /></a>
+
+//                 <a href="#" class="see-source"
+//                   ><img src="./images/Enabled.png" alt="see source"
+//                 /></a>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//     `;
+//     }
+//   }
+//   generateProduct();
+// });
+
+// modal.addEventListener('click', (e) => {
+//   if (e.target.id === 'close-btn') {
+//     modal.style.display = 'none';
+//   }
+//   generateProduct();
+// });
+
+// generateProduct();
--- script.js
+++ script.js
@@ -1,24 +1,215 @@
-const menuEl = document.querySelector('.desktop-link');
-const hamburgerButton = document.querySelector('.hamburger');
+const deskEl = document.querySelector('.desktop-menu');
+const humbergerIcon = document.querySelector('.humberger');
 const closeIcon = document.querySelector('.close');
 const navListEl = document.querySelectorAll('.nav-list');
 
-hamburgerButton.addEventListener('click', () => {
-  menuEl.classList.toggle('active');
-  hamburgerButton.classList.toggle('hide');
+humbergerIcon.addEventListener('click', () => {
+  deskEl.classList.toggle('active');
+  humbergerIcon.classList.toggle('hide');
   closeIcon.classList.toggle('show');
 });
 
 closeIcon.addEventListener('click', () => {
-  menuEl.classList.toggle('active');
-  hamburgerButton.classList.toggle('hide');
+  deskEl.classList.toggle('active');
+  humbergerIcon.classList.toggle('hide');
   closeIcon.classList.toggle('show');
 });
 
 navListEl.forEach((element) => {
   element.addEventListener('click', () => {
-    menuEl.classList.toggle('active');
-    hamburgerButton.classList.toggle('hide');
+    deskEl.classList.toggle('active');
+    humbergerIcon.classList.toggle('hide');
     closeIcon.classList.toggle('show');
   });
 });
+
+const projects = [
+  {
+    name: 'Tonic',
+    des: ['Canopy', 'backend', '2015'],
+    img: 'images/snapshoot1.png',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    desc1:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies1: ['github', 'ruby', 'Bootstrap'],
+    liveVersion: 'https://gmanlove.github.io/My-Portfolio/ ',
+    sourceCode: 'https://github.com/Megagig/My-Portfolio',
+  },
+  {
+    name: 'Multi-Post Stories',
+    des: ['Canopy', 'backend', '2015'],
+    img: 'images/snapshoot2.png',
+    description:
+      'Experimental content creation feature that allows users to add an existing story over the course of a day without spamming their friends.',
+    desc1:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies1: ['github', 'ruby', 'Bootstap'],
+    liveVersion: 'https://megagig.github.io/My-Portfolio/ ',
+    sourceCode: 'https://github.com/Megagig/My-Portfolio',
+  },
+  {
+    name: 'Facebook 360',
+    des: ['Canopy', 'backend', '2015'],
+    img: 'images/snapshoot3.png',
+    description:
+      'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos pn Gear VR.',
+    desc1:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies1: ['github', 'ruby', 'Bootstrap'],
+    liveVersion: 'https://megagig.github.io/My-Portfolio/ ',
+    sourceCode: 'https://github.com/Megagig/My-Portfolio',
+  },
+  {
+    name: 'Uber Navigation',
+    des: ['Canopy', 'backend', '2015'],
+    img: 'images/snapshoot4.png',
+    description:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    desc1:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    technologies1: ['github', 'ruby', 'Bootsrap'],
+    liveVersion: 'https://megagig.github.io/My-Portfolio/',
+    sourceCode: 'https://github.com/Megagig/My-Portfolio',
+  },
+];
+
+const workpage = document.querySelector('#workpage');
+for (let i = 0; i < projects.length; i += 1) {
+  workpage.innerHTML += `
+  <div class="card-proj">
+  <img src="${projects[i].img}" alt="project card image" />
+  <div class="project-card-iterms flex column">
+    <h2 class="card-title">${projects[i].name}</h2>
+    <div class="card-lists">
+      <p class="canopy">${projects[i].des[0]}</p>
+      <div class="round"></div>
+      <p>${projects[i].des[1]}</p>
+      <div class="round"></div>
+      <p>${projects[i].des[2]}</p>
+    </div>
+    <div class="card-note">
+      <p>
+      ${projects[i].description}
+      </p>
+    </div>
+    <div class="card-tag">
+      <ul class="card-skills flex">
+        <li>${projects[i].technologies[0]}</li>
+        <li>${projects[i].technologies[1]}</li>
+        <li>${projects[i].technologies[2]}</li>
+      </ul>
+    </div>
+    <button type="button" id="btn-${i}">See Project</button>
+  </div>
+  </div>
+  `;
+}
+
+const modal = document.getElementById('modal1');
+
+for (let j = 0; j < projects.length; j += 1) {
+  const seeBtn = document.querySelectorAll(`[id=btn-${j}]`);
+  seeBtn.forEach((bt) => {
+    bt.addEventListener('click', () => {
+      modal.style.display = 'block';
+      modal.innerHTML = `
+      <div class="popup-container block-popup">
+      <div id="popup-modal" class="pop-content">
+        <div class="headline-popup">
+          <h1>${projects[j].name}</h1>
+          <a href="#" role="button" onclick="closePopup()">
+            <div class="popup-clsbtn">x</div>
+          </a>
+        </div>
+      </div>
+      <ul class="small-tags">
+        <li class="canopy-sm-tag">${projects[j].des[0]}</li>
+        <li>
+          <div class="round-popup"></div>
+        </li>
+        <li class="second-sm-tag">${projects[j].des[1]}</li>
+        <li>
+          <div class="round-popup"></div>
+        </li>
+        <li class="second-sm-tag">${projects[j].des[2]}</li>
+      </ul>
+      <div class="popup-image-container">
+        <img src="${projects[j].img}" alt="pop-image" />
+      </div>
+      <div class="buttom-content-container">
+        <div class="paragraph-description">
+          <p class="paragraph-description-desktop">
+            ${projects[j].desc1}
+          </p>
+        </div>
+        <div class="action-content">
+          <div class="popup-tags">
+            <ul class="first">
+            <li>${projects[j].technologies[0]}</li>
+            <li>${projects[j].technologies[1]}</li>
+            <li>${projects[j].technologies[2]}</li>
+            </ul>
+            <ul class="second">
+            <li>${projects[j].technologies1[0]}</li>
+            <li>${projects[j].technologies1[1]}</li>
+            <li>${projects[j].technologies1[2]}</li>
+            </ul>
+          </div>
+          <div class="divider-popup">
+            <img src="images/Vector4.png" alt="Divider popup" />
+          </div>
+          <div class="popup-button-action">
+            <button type="submit">
+              <div class="source-code">
+                <a href="">See live </a>
+              </div>
+              <div class="deploy-icon">
+                <img src="images/Icon.png" alt="deployment icon" class="deploy-icon-popup" />
+              </div>
+            </button>
+            <button class="submit-github">
+              <div class="live-version">
+                <a href="">See Source </a>
+              </div>
+              <div class="github-img">
+                <img src="images/Vector.png" alt="github icon" class="github-icon-popup" />
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+      `;
+
+      const closeBtn = document.querySelector('.popup-clsbtn');
+      closeBtn.addEventListener('click', () => {
+        const modal = document.querySelector('.modal');
+        modal.style.display = 'none';
+      });
+    });
+  });
+}
+
+// ---------------------------Contact form validation---------------------
+const formContact = document.getElementById('form');
+
+function validateEmail() {
+  const email = document.getElementById('email').value;
+  const error = document.getElementById('errorMessage');
+  if (email.toLowerCase() !== email) {
+    error.innerHTML = 'Please enter your email in lowercase';
+    error.style.display = 'block';
+    error.style.color = '#FFFF';
+    error.style.fontSize = 'larger';
+    return false;
+  }
+  document.getElementById('errorMessage').innerHTML = '';
+  return true;
+}
+
+formContact.addEventListener('submit', validateEmail);
